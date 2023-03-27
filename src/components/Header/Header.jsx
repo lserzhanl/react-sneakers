61078af2a4bc7c38f69f7a97abe304a164c6973a
lserzhanl <zhumagazhayev.s@gmail.com>
2023-03-28 01:42:46 +0600

    fixed: Не отображались картинки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-function Header({ onClickCart}) {
+function Header({ onClickCart }) {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img width={40} height={40} src="img/logo.png" />
+        <img width={40} height={40} src="img/logo.png" alt="Logo" />
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кросовок</p>
@@ -10,15 +10,15 @@ function Header({ onClickCart}) {
       </div>
       <ul className="d-flex">
         <li className="mr-30 cu-p" onClick={onClickCart}>
-          <img width={18} height={18} src="img/cart.svg" />
+          <img width={18} height={18} src="img/cart.svg" alt="Cart" />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img width={18} height={18} src="img/user.svg  " />
+          <img width={18} height={18} src="img/user.svg" alt="User" />
         </li>
       </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
